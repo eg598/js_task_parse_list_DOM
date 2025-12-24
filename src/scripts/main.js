d601c59e4272dcb13d64a5a7abe8aae558838e0a
Yegor <egor598000@gmail.com>
2025-12-24 23:57:51 +0100

    fixed task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const ul = document.querySelector('ul');
-const list = Array.from(document.querySelectorAll('li'));
+const list = sortList(Array.from(document.querySelectorAll('li')));
 
-list.sort((item1, item2) => {
-  const a = parseSalary(item1.dataset.salary);
-  const b = parseSalary(item2.dataset.salary);
+function sortList(arr) {
+  return arr.sort((item1, item2) => {
+    const a = parseSalary(item1.dataset.salary);
+    const b = parseSalary(item2.dataset.salary);
 
-  return b - a;
-});
+    return b - a;
+  });
+}
 
 for (const item of list) {
   ul.insertAdjacentElement('beforeend', item);
