ee78f4c3e14ac88f78e23c3d9e2bff286cad586d
Yegor <egor598000@gmail.com>
2025-12-24 15:59:39 +0100

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const list = Array.from(document.querySelectorAll('li'));
+
+list.sort((item1, item2) => {
+  const a = parseSalary(item1.dataset.salary);
+  const b = parseSalary(item2.dataset.salary);
+
+  return b - a;
+});
+
+for (const item of list) {
+  ul.insertAdjacentElement('beforeend', item);
+}
+
+function parseSalary(number) {
+  let result = '';
+  const arr = Array.from(number);
+  const map = '1234567890';
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map.includes(arr[i])) {
+      result += arr[i];
+    }
+  }
+
+  return Number.parseInt(result);
+}
